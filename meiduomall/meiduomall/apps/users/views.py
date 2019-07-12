74fd7b332c8d4427ba0a3fcbff5d7ecd60ac22a1
yeyuning <yeyuning1@gmail.com>
2019-07-12 14:16:04 +0800

    ‘增加手机验证功能’

--- meiduomall/meiduomall/apps/users/views.py
+++ meiduomall/meiduomall/apps/users/views.py
@@ -24,7 +24,7 @@ class UsernameCountView(View):
         """
         # 获取数据库中该用户名对应的个数
         count = User.objects.filter(username=username).count()
-        print(count)
+
         # 拼接参数, 返回:
         return JsonResponse({'code': 0, 'errmsg': 'OK', 'count': count})
 
@@ -32,4 +32,5 @@ class UsernameCountView(View):
 class MobileCountView(View):
     def get(self, request, mobile):
         count = User.objects.filter(mobile=mobile).count()
+        print(count)
         return JsonResponse({'code': 0, 'errmsg': 'OK', 'count': count})
