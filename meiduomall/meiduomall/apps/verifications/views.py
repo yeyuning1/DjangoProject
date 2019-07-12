e0388ca54da8fef5fbfb3993c5ea25270172618e
yeyuning <yeyuning1@gmail.com>
2019-07-12 21:43:05 +0800

    ‘完成登陆注册功能’

--- meiduomall/meiduomall/apps/verifications/views.py
+++ meiduomall/meiduomall/apps/verifications/views.py
@@ -46,9 +46,18 @@ class SendSmsCodeView(View):
         if not all([image_code_cli, uuid]):
             return JsonResponse({'code': RETCODE.NECESSARYPARAMERR,
                                  'errmsg': '缺少必传参数'})
-        # 3.从redis中取得图形验证码
+        # 检查是否频繁发送短信验证码请求
+        # 连接redis
         redis_conn = get_redis_connection('verify_code')
+        # 检查send_flag
+        send_flag = redis_conn.get('send_flag_%s' % mobile)
+        if send_flag:
+            return JsonResponse({{'code': RETCODE.NECESSARYPARAMERR,
+                                  'errmsg': '发送短信过于频繁'}})
+
+        # 3.从redis中取得图形验证码
         image_code_server = redis_conn.get('image_code_%s' % uuid)
+
         # 4.对比、校验图形验证码
         if image_code_server is None:
             return JsonResponse({'code': RETCODE.IMAGECODEERR,
@@ -67,8 +76,15 @@ class SendSmsCodeView(View):
         sms_code = '%06d' % random.randint(0, 999999)
         # logger.info(sms_code)
         print(sms_code)
+
+        # 使用容联云平台给用户发送短信
         # CCP().send_template_sms(mobile,
         #                         [sms_code, crons.SMS_CODE_EXPIRE_M], crons.SMS_CODE_EXPIRE_TEMPLATES)
-        # 6.保存手机验证码
+
+        # 保存手机验证码
         redis_conn.setex('sms_code_%s' % mobile, crons.SMS_CODE_EXPIRE_S, sms_code)
+        # 重新写入send_flag
+        # 60s内是否重复发送的标记
+        # SEND_SMS_CODE_INTERVAL = 60
+        redis_conn.setex('send_flag_%s' % mobile, crons.SEND_SMS_CODE_INTERVAL, 1)
         return JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信成功'})
