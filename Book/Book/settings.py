765c6243c55597f5baa7eb98e5621eb3cb12fb70
yeyuning1 <yeyuning1@gmail.com>
2019-07-29 21:58:17 +0800

    配置mysql数据库

--- Book/Book/settings.py
+++ Book/Book/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'Book.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'book',
+        'USER': 'yeyuning',
+        'PASSWORD': 'yeyuning',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
