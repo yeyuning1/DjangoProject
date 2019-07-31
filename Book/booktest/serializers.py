15f0435d230ae9a5c52360df46eecf56ca36741d
yeyuning1 <yeyuning1@gmail.com>
2019-07-31 18:47:16 +0800

    使用rest framework 风格

--- Book/booktest/serializers.py
+++ Book/booktest/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
-from booktest.models import BookInfo
+from booktest.models import BookInfo, HeroInfo
+
+
+class HeroInfoSerializer(serializers.ModelField):
+    class Meta:
+        model = HeroInfo
+        exclude = ('is_delete',)
 
 
 class BookInfoSerializer(serializers.ModelSerializer):
@@ -8,4 +14,4 @@ class BookInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BookInfo
-        fields = '__all__'
+        exclude = ('is_delete',)
--- Book/booktest/views.py
+++ Book/booktest/views.py
@@ -1,56 +1,47 @@
-import json
-from django.http import JsonResponse, HttpResponse
-from django.views import View
-from rest_framework import status
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from booktest.fortest import HeroInfoSerializer
-from booktest.models import BookInfo, HeroInfo
+# class BookInfoViewSet(GenericAPIView):
+#
+#     queryset = BookInfo.objects.all()
+#     serializer_class = BookInfoSerializer
+#
+#     def get(self, request):
+#         list = self.get_queryset()
+#         serializer = self.get_serializer(list, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     def post(self, request):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#
+# class HeroInfoViewSet(GenericAPIView):
+#     queryset = HeroInfo.objects.all()
+#     serializer_class = HeroInfoSerializer
+#
+#     def get(self, request, pk):
+#         instance = self.get_object()
+#         serializer = self.get_serializer(instance)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     def put(self, request, pk):
+#         instance = self.get_object()
+#         serializer = self.get_serializer(instance, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#
+#     def delete(self, request, pk):
+#         instance = self.get_object()
+#         instance.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+from booktest.models import BookInfo
 from booktest.serializers import BookInfoSerializer
 
 
-class BookInfoViewSet(GenericAPIView):
-    """视图集"""
-    queryset = BookInfo.objects.all()
+class BookInfoViewSet(ModelViewSet):
     serializer_class = BookInfoSerializer
+    queryset = BookInfo.objects.all()
 
-    def get(self, request):
-        list = self.get_queryset()
-        serializer = self.get_serializer(list, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class HeroInfoViewSet(GenericAPIView):
-    queryset = HeroInfo.objects.all()
-    serializer_class = HeroInfoSerializer
-
-    def get(self, request, pk):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def put(self, request, pk):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        instance = self.get_object()
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
