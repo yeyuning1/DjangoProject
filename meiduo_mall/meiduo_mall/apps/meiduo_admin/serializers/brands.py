a2434f1bde812098787c61da9a27beb0e7f8783f
yeyuning1 <yeyuning1@gmail.com>
2019-08-08 21:27:38 +0800

    完成品牌管理功能

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/brands.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/brands.py
@@ -7,5 +7,8 @@ class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         exclude = ('create_time', 'update_time')
+
     def validate_name(self, value):
-        Brand.object.get(name=value)
+        brand = Brand.object.get(name=value)
+        if brand:
+            raise serializers.ValidationError('改品牌已存在')
